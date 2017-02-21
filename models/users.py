1de6db46aeaf0bed2e3c7b4e0f7945a023817692
naha0102 <ansara@ehapi.com>
2017-02-21 09:27:27 +0400

    initial commit

--- models/users.py
+++ models/users.py
@@ -3,51 +3,24 @@ from openerp.osv import fields, osv
 class manage_group(osv.osv):
 
 	_inherit="res.groups"
-<<<<<<< HEAD
-	_columns={ 
-	'share_group': fields.boolean('Share Group'),
-	'portal' : fields.boolean('Portal'),
-=======
-	_columns={ 'share_group': fields.boolean('Share Group'),
-				'portal' : fields.boolean('Portal'),
->>>>>>> 5102f8d9270ca85cbcf3e3aab4312ca87f6738ba
-	}
 	
 manage_group()
 
 
 class manage_users(osv.osv):
-	
 	_inherit="res.users"
-
 	_columns = {
 	'company':fields.many2one('multi_company.default','Company',required=True),
 	'customer':fields.boolean('Customer'),
 	'is_team_leader':fields.boolean('Is Team Leader??'),
 	# 'tuser_id':fields.char('User ID',readonly=True, size=64),
-<<<<<<< HEAD
 	'contact_num':fields.char('Contact Number',size=32),
 	'joining_date':fields.date('Joining Date'),
 	'comments':fields.text('Comments'),
 	'team_leader':fields.char('Team Leader',size=32),
-=======
-	'contact_num':fields.integer('Contact Number'),
-	'joining_date':fields.date('Joining Date'),
-	'comments':fields.text('Comments'),
-	'team_leader':fields.char('Team Leader'),
->>>>>>> 5102f8d9270ca85cbcf3e3aab4312ca87f6738ba
 	'role':fields.char('Role',size=32),
 	'survey_limit':fields.integer('Limit of Surveys'),
 	'customer_ids':fields.many2one('customer.info','Allowed Customers'),
-<<<<<<< HEAD
-	# 'password1' : fields.char('Password', invisible=True,required=True,size=64),
-=======
-	'password1' : fields.char('Password', invisible=True,required=True,size=64),
-<<<<<<< HEAD
-=======
->>>>>>> d3b3edcb2c18353c24a2e54b2718131d2448e34d
-	'sharing' : fields.selection([('false','False'),('user_id','User')],'Sharing'),
->>>>>>> 5102f8d9270ca85cbcf3e3aab4312ca87f6738ba
 	}
 
 manage_users()
