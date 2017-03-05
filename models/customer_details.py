1b67d3238e928baf3d748a29b936509d54607844
surya muppalla <surya@anipr.in>
2017-03-05 10:32:08 +0400

    done

--- models/customer_details.py
+++ models/customer_details.py
@@ -1,4 +1,13 @@
 from openerp.osv import fields, osv
+from openerp.tools.translate import _
+import datetime
+import time
+import random
+import re
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from openerp import  tools
 
 # customer details class
 class customer_details(osv.osv):
@@ -33,6 +42,50 @@ class customer_details(osv.osv):
 	}
 	
 
+	def send_invitation(self,cr,uid,ids,context=None):
+		cust_obj = self.browse(cr,uid,ids,context=None)[0]
+		if not cust_obj.cont_per_e1:
+			raise osv.except_osv(_('No Email Provided for this customer'),_("Please give a Valid email address !") )
+			return False
+		name=cust_obj.cust_name
+		if re.search(r'\s',cust_obj.cust_name):
+			name=cust_obj.cust_name.replace(" ", "_")
+		chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+		password = ''.join(random.choice(chars) for i in xrange(6))
+		c_id = self.pool.get('res.users').search(cr,uid,[('name','=',cust_obj.cust_name)])
+		gr_id = self.pool.get('res.groups').search(cr,uid,[('name','=','Customer')])
+		del_id = self.pool.get('res.groups').search(cr,uid,[('name','=','Employee')])
+		if c_id:
+			raise osv.except_osv(_('You are already created a User for this customer'),_("Please recheck your users list via Users Menu.") )
+			return False
+		val={'name':cust_obj.cust_name,'login':name,'password':str(password),'role':'Customer'}
+		new_id = self.pool.get('res.users').create(cr,uid,val,context=None)
+		cr.execute('insert into  res_groups_users_rel (uid, gid) values(%s,%s)', (new_id, gr_id[0]))
+		cr.execute('DELETE from  res_groups_users_rel where uid = %s and gid = %s', (new_id, del_id[0]))
+		mail_ids = self.pool.get('ir.mail_server').search(cr,uid,[('active','=','True')])
+		mail_obj = self.pool.get('ir.mail_server').browse(cr,uid,mail_ids)[0]
+		username = mail_obj.smtp_user
+		pwd = mail_obj.smtp_pass
+		host = mail_obj.smtp_host
+		port = mail_obj.smtp_port
+		fromaddr = username
+		server = smtplib.SMTP(host+':'+'587')
+		server.ehlo()
+		server.starttls()
+		server.ehlo()
+		server.login(username, pwd)
+		msg = MIMEMultipart()
+		msg['From'] = fromaddr
+		msg['Subject'] = 'Regarding Account Details For TransTechERP'
+		toaddr = cust_obj.contact_email
+		msg['To'] = toaddr
+		text = ('<p><h2>Hello, %s</h2> Your account has been created in Transtech ERP as User</p><p><b>Your login credentials are given below:-</b></p><p>Username: %s</p><p>Password: %s</p><p>To login click on below link</p><p><a href="http://162.243.21.15:8069/">Click Here</a></p>')%(cust_obj.cust_name,name,password)
+		body = MIMEText(text, _subtype='html')
+		msg.attach(body)
+		res = server.sendmail(fromaddr, toaddr, msg.as_string())
+		server.quit()
+
+		return True
 
 customer_details()
 
