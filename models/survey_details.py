2acb28667e2f3ca00e6a2b3ea8a9e0b2d24c8ed5
heena012 <heena@ehapi.com>
2017-03-08 18:02:00 +0400

    done

--- models/survey_details.py
+++ models/survey_details.py
@@ -386,13 +386,13 @@ class survey_details(osv.osv):
 
 	
 
-	# def print_survey(self, cr, uid, ids, context=None):
-	# 	'''
-	# 	This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
-	# 	'''
-	# 	assert len(ids) == 1, 'This option should only be used for a single id at a time'
-	# 	self.signal_workflow(cr, uid, ids, 'quotation_sent')
-	# 	return self.pool['report'].get_action(cr, uid, ids, 'atm.print_survey', context=context)
+	def print_survey(self, cr, uid, ids, context=None):
+		'''
+		This function prints the sales order and mark it as sent, so that we can see more easily the next step of the workflow
+		'''
+		assert len(ids) == 1, 'This option should only be used for a single id at a time'
+		self.signal_workflow(cr, uid, ids, 'quotation_sent')
+		return self.pool['report'].get_action(cr, uid, ids, 'atm.print_survey', context=context)
 	 
 	
 
